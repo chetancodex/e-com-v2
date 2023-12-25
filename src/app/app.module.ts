fc3b59d90fe849560c629395b4caf271318f10fb
chetancodex <ckkumar419@gmail.com>
2023-12-25 16:26:52 +0530

    update navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './common/nav/nav.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
