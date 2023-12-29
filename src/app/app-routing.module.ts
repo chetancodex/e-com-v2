6a8cf3a2235fb766e41ba87a00ec8e77cf3c6029
chetancodex <ckkumar419@gmail.com>
2023-12-29 17:33:07 +0530

    support page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { SupportComponent } from './common/support/support.component';
 
 const routes: Routes = [
-  { path : "" , component: HomeComponent},
-  {path : "products", component : ProductsComponent}
+  { path: "", component: HomeComponent },
+  { path: "products", component: ProductsComponent },
+  { path: "support", component: SupportComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './common/nav/nav.component';
 import { ProductsComponent } from './products/products.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SupportComponent } from './common/support/support.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ProfileComponent } from './profile/profile.component';
     NavComponent,
     ProductsComponent,
     ProfileComponent,
+    SupportComponent,
   ],
   imports: [
     BrowserModule,
